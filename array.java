33a51116e077eeea8ec043757821226ffa0f4951
Mamatha <mbanne001@gmail.com>
2026-02-27 08:40:19 -0600

    sum of array

---
+++ array.java
@@ -0,0 +1,22 @@
+
+
+public class array {
+    public static void main(String[] args) {
+        
+        int[] arr={10,30,50,80,90};
+        int sum=0;
+
+           for (int i = 0; i < arr.length; i++) {
+               
+          
+            sum=sum+arr[i];
+           }
+           System.out.println(sum);
+
+    
+        }
+     
+
+    }
+    
+
