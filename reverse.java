a809083af9016c1ae1aaef7342e3acf9168d258a
Mamatha <mbanne001@gmail.com>
2026-02-28 09:55:50 -0600

    commit

---
+++ reverse.java
@@ -0,0 +1,13 @@
+public class reverse {
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40,50};
+        int j=arr.length;
+        for (int i = 0; i < arr.length; i++) {
+            j=j-1;
+            System.out.println(arr[j]);
+            
+        }
+       
+    }
+    }
+
